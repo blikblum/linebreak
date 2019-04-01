c7e600320efbed832bc3fc3eb635cabc4ac39bad
Luiz Américo <luizamericop@gmail.com>
2019-04-01 09:44:37 -0300

    Do not convert classes.trie file to base64

--- src/linebreaker.js
+++ src/linebreaker.js
@@ -1,11 +1,10 @@
 let AI, AL, BA, BK, CB, CJ, CR, ID, LF, NL, NS, SA, SG, SP, WJ, XX;
 const UnicodeTrie = require('unicode-trie');
 const fs = require('fs');
-const base64 = require('base64-js');
 ({BK, CR, LF, NL, CB, BA, SP, WJ, SP, BK, LF, NL, AI, AL, SA, SG, XX, CJ, ID, NS} = require('./classes'));
 const {DI_BRK, IN_BRK, CI_BRK, CP_BRK, PR_BRK, pairTable} = require('./pairs');
 
-const data = base64.toByteArray(fs.readFileSync(__dirname + '/classes.trie', 'base64'));
+const data = fs.readFileSync(__dirname + '/classes.trie');
 const classTrie = new UnicodeTrie(data);
 
 const mapClass = function(c) {
